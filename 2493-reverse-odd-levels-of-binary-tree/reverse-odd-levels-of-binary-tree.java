cd4e25c1877d1a2158b4cfea6d99a026c0a7524e
kushkumar7668 <kushkumar16052004@gmail.com>
2025-08-27 12:17:51 +0530

    Time: 1 ms (69.48%) | Memory: 48.7 MB (40.20%) - LeetSync

--- 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
@@ -17,23 +17,36 @@ class Solution {
     int depth = 0;
     public TreeNode reverseOddLevels(TreeNode root) {
         if(root == null) return null;
-        invert(root.left, root.right, depth+1);
+        dfs(root.left, root.right, depth+1);
         return root;
     }
 
-    public void invert(TreeNode leftNode, TreeNode rightNode, int depth){
+    public void dfs(TreeNode node1, TreeNode node2, int depth){
         
-        if(leftNode == null || rightNode == null)return;
+        if(node1 == null || node2 == null)return;
 
-    // you actually have to change the reference of left and right sub-tree not the value only.
-        if((leftNode!=null || rightNode!=null) && depth%2 == 1){
-            int temp = leftNode.val;
-            leftNode.val = rightNode.val;
-            rightNode.val = temp;
+          
+    // you have to change the value only here
+        if(depth%2 == 1){
+            int temp = node1.val;
+            node1.val = node2.val;
+            node2.val = temp;
         }
         
-        invert(leftNode.left, rightNode.right, depth+1);
-        invert(leftNode.right, rightNode.left, depth+1);
+        // for this logic refer to codestorywithMIK at 16:30
+        dfs(node1.left, node2.right, depth+1);
+        dfs(node1.right, node2.left, depth+1);
+
+            //              2                       level 0
+            //          /        \
+            //        3              5              level 1
+            //     /     \         /    \
+            //    8       13      21     34         level 2
+            //  /  \     /  \    /  \    /  \
+            // 1    2   3    4   5   6  7    8      level 3
+
+    //  at level 3 swap left(8) ka left(1) with right(34) ka right(8) by recursion
+
 
         return ;
     }
