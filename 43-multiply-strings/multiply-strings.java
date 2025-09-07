453b90738d2d16e0f4cd28d3887ee1e7ccbdfe93
kushkumar7668 <kushkumar16052004@gmail.com>
2025-09-07 23:54:30 +0530

    Time: 15 ms (5.65%) | Memory: 44.9 MB (6.91%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.java
@@ -0,0 +1,54 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) return "0";
+
+        String[] arr = new String[num1.length()];
+        int k = 0;
+
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            StringBuilder sb = new StringBuilder();
+            int carry = 0;
+            int d1 = num1.charAt(i) - '0';
+
+            for (int j = num2.length() - 1; j >= 0; j--) {
+                int d2 = num2.charAt(j) - '0';
+                int x = d1 * d2 + carry;
+                sb.append(x % 10);
+                carry = x / 10;
+            }
+            if (carry > 0) sb.append(carry);
+
+            sb.reverse();
+            // add trailing zeros for place value
+            for (int z = 0; z < (num1.length() - 1 - i); z++) {
+                sb.append("0");
+            }
+
+            arr[k++] = sb.toString();
+        }
+
+        String ans = "0";
+        for (int i = 0; i < k; i++) {
+            ans = addStrings(ans, arr[i]);
+        }
+        return ans;
+    }
+
+    public String addStrings(String num1, String num2) {
+        int i = num1.length() - 1, j = num2.length() - 1;
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int d1 = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int d2 = j >= 0 ? num2.charAt(j) - '0' : 0;
+            int sum = d1 + d2 + carry;
+            sb.append(sum % 10);
+            carry = sum / 10;
+            i--;
+            j--;
+        }
+
+        return sb.reverse().toString();
+    }
+}
