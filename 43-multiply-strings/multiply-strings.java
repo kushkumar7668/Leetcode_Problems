5bcdb39599aff3aa4d114a9d5fc9b972887b545c
kushkumar7668 <kushkumar16052004@gmail.com>
2025-09-08 00:08:11 +0530

    Time: 3 ms (54.94%) | Memory: 42.2 MB (79.07%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -1,5 +1,40 @@
 class Solution {
     public String multiply(String num1, String num2) {
+        int m=num1.length();
+        int n=num2.length();
+        int[] res=new int[m+n];
+
+        for(int i= m-1;i>=0;i--){
+            int d1 =num1.charAt(i)-'0';
+            for(int j=n-1;j>=0;j--){
+                int d2 =num2.charAt(j) -'0';
+                int mul =d1*d2;
+                int p1 =i+j;     //stores carry/ 10th place
+                int p2 = i+j+1;  // atores product's unit digit 
+                int sum =mul+res[p2];
+                res[p2] =sum%10;
+                res[p1] +=sum/10;
+            }
+        }
+        StringBuilder sb =new StringBuilder();
+        for(int num: res){
+            if(!(sb.length() == 0 && num == 0)){
+                sb.append(num);
+            }
+        }
+        return sb.length() == 0 ? "0" :sb.toString();
+    }
+}
+
+
+
+
+
+
+
+/*
+class Solution {
+    public String multiply(String num1, String num2) {
         if (num1.equals("0") || num2.equals("0")) return "0";
 
         String[] arr = new String[num1.length()];
@@ -52,3 +87,13 @@ class Solution {
         return sb.reverse().toString();
     }
 }
+
+
+*/
+
+
+
+
+
+
+
