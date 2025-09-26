bbe03d993a63ce553b7c336dabcf0e455c93d37f
kushkumar7668 <kushkumar16052004@gmail.com>
2025-09-26 23:44:48 +0530

    Time: 485 ms (5.01%) | Memory: 65.5 MB (5.31%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,31 +1,24 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        Deque<Integer> dq=new LinkedList<>();
-        //addFirst
-        //addLast
-        //removeFirst
-        //getFirst
-        //getLast
-        int n = nums.length;
-        int j=0;
-        int arr[]=new int[n-k+1];
-        for(int i=0;i<k;i++){
-            while(!dq.isEmpty() && nums[i]>=nums[dq.getLast()]){
-                dq.removeLast();    // remove last
-            }
-            dq.addLast(i);  // add last
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int n=nums.length;
+        int[] result = new int[n - k + 1];
+        for (int i = 0; i < k; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
-        arr[j++]=nums[dq.getFirst()];
-        for(int i=k;i<n;i++){   
-            if(dq.getFirst()<=(i-k)){
-                dq.removeFirst();   // remove first
+        
+        result[0] = map.lastKey();
+        for (int i = k; i < n; i++) {
+            int rem = nums[i - k];
+            map.put(rem, map.get(rem) - 1);
+            if (map.get(rem) == 0) {
+                map.remove(rem);
             }
-            while(!dq.isEmpty() && nums[i]>=nums[dq.getLast()]){
-                dq.removeLast();    //remove last
-            }
-            dq.addLast(i);
-            arr[j++]=nums[dq.getFirst()];
+            int add = nums[i];
+            map.put(add, map.getOrDefault(add, 0) + 1);
+            result[i - k + 1] = map.lastKey();
         }
-        return arr;
+
+        return result;
     }
 }
