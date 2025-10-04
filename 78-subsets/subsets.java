32feb6f229e2d3e4956846fcde3333e7d9c50bdd
kushkumar7668 <kushkumar16052004@gmail.com>
2025-10-04 11:09:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (47.81%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -3,18 +3,31 @@ class Solution {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> ll = new ArrayList<>();
 
-        helper(nums, 0, res, ll);
+        helper(nums, 0, ll, res);
         return res;        
     }
 
-    private void helper(int[] nums, int index, List<List<Integer>> res, List<Integer> ll) {
-       res.add(new ArrayList<>(ll));
+    // private void helper(int[] nums, int index, List<Integer> ll, List<List<Integer>> res) {
+    //    res.add(new ArrayList<>(ll));
 
-       for(int i = index; i<nums.length; i++){
-        ll.add(nums[i]);
-        helper(nums, i+1, res, ll);
-        ll.remove(ll.size()-1);
+    //    for(int i = index; i<nums.length; i++){
+    //     ll.add(nums[i]);
+    //     helper(nums, i+1, res, ll);
+    //     ll.remove(ll.size()-1);
 
-       }
-    }    
+    //    }
+    // }    
+
+    private void helper(int[] nums, int index, List<Integer> ll, List<List<Integer>> res){
+        if(index == nums.length){
+            res.add(new ArrayList<>(ll));
+            return;
+        }
+
+        ll.add(nums[index]);
+        helper(nums, index+1, ll, res);
+
+        ll.remove(ll.size() -1);
+        helper(nums, index+1, ll, res);
+    }
 }
