49124a68bc078af71b407c0226ae9a5b5e0d822b
kushkumar7668 <kushkumar16052004@gmail.com>
2025-10-05 19:51:47 +0530

    Time: 1 ms (99.45%) | Memory: 57.5 MB (8.36%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,14 +1,29 @@
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+//         int ans=Integer.MIN_VALUE;
+//         int sum=0;
+//         for(int i=0;i<nums.length;i++){
+//             sum+= nums[i];
+//             ans = Math.max(sum, ans);
+//             if(sum<0){
+//                 sum=0;      // kadane's algo
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int maxSubArray(int[] nums) {
-        int ans=Integer.MIN_VALUE;
-        int sum=0;
-        for(int i=0;i<nums.length;i++){
-            sum+= nums[i];
-            ans = Math.max(sum, ans);
-            if(sum<0){
-                sum=0;      // kadane's algo
+        int storage[] = new int[nums.length];
+        int max = nums[0];
+        storage[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            storage[i] = Math.max(storage[i - 1] + nums[i], nums[i]);
+            if (storage[i] > max) {
+                max = storage[i];
             }
         }
-        return ans;
+        return max;
     }
 }
