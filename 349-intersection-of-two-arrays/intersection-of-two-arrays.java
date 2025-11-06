60dc6d6aed676761b3b9103be30648608d9536c1
kushkumar7668 <kushkumar16052004@gmail.com>
2025-11-07 03:13:51 +0530

    Time: 2 ms (97.58%) | Memory: 44.7 MB (5.99%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,24 +1,54 @@
+// class Solution {
+//     public int[] intersection(int[] nums1, int[] nums2) {
+//         Arrays.sort(nums1);
+//         Arrays.sort(nums2);
+//         HashSet<Integer> set = new HashSet<>();
+//         for(int i =0; i<nums1.length; i++){
+//             for(int j = 0; j<nums2.length; j++){
+//                 if(nums1[i] == nums2[j] && !set.contains(nums1[i])){
+//                     set.add(nums1[i]);
+//                 }
+//             }
+//         }
+
+//         int[] arr = new int[set.size()];
+//         int i =0;
+//         for(int x : set){
+//             arr[i] = x;
+//             i++;
+//         }
+        
+
+//         return arr;
+//     }
+// }
+
+
+
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        HashSet<Integer> set = new HashSet<>();
-        for(int i =0; i<nums1.length; i++){
-            for(int j = 0; j<nums2.length; j++){
-                if(nums1[i] == nums2[j] && !set.contains(nums1[i])){
-                    set.add(nums1[i]);
-                }
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> result = new HashSet<>();
+
+        // Add all elements of nums1 into set1
+        for (int num : nums1) {
+            set1.add(num);
+        }
+
+        // Check elements of nums2
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                result.add(num);
             }
         }
 
-        int[] arr = new int[set.size()];
-        int i =0;
-        for(int x : set){
-            arr[i] = x;
-            i++;
+        // Convert result set to int array
+        int[] ans = new int[result.size()];
+        int i = 0;
+        for (int num : result) {
+            ans[i++] = num;
         }
-        
 
-        return arr;
+        return ans;
     }
 }
