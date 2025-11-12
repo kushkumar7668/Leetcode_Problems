b1ccfb880cac2749b80db4965b9ed51a4eff3503
kushkumar7668 <kushkumar16052004@gmail.com>
2025-11-13 02:43:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (6.47%) - LeetSync

--- 86-partition-list/partition-list.java
+++ 86-partition-list/partition-list.java
@@ -10,26 +10,28 @@
  */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        ListNode dummy = head;
-        ListNode small = new ListNode();
-        ListNode large = new ListNode();
-        ListNode sp = small;
-        ListNode lp = large;
+        ListNode left = new ListNode(0);
+        ListNode right = new ListNode(0);
+        // here the catch is that left and right node are not real node they are dummy nodes which are pointing towards two nodes 0.   ( left, leftTail ) & ( right, rightTail) are pointing towards same nodes (0) 
+        
+        ListNode leftTail = left;
+        ListNode rightTail = right;
+        
         while(head != null){
             if(head.val < x){
-                sp.next = head;
-                sp = sp.next;
+                leftTail.next = head;
+                leftTail = leftTail.next;
             }
             else{
-                lp.next = head;
-                lp = lp.next;
+                rightTail.next = head;
+                rightTail = rightTail.next;
             }
             head = head.next;
         }
-        lp.next = null;
-        sp.next = large.next;
         
-        return small.next;
+        leftTail.next = right.next;
+        rightTail.next = null;
         
+        return left.next;
     }
 }
