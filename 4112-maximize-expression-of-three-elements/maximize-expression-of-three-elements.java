e79177833623e5552ea61790517367d91d8bde05
kushkumar7668 <kushkumar16052004@gmail.com>
2025-11-16 12:29:15 +0530

    Time: 6 ms (100.00%) | Memory: 45.6 MB (100.00%) - LeetSync

---
+++ 4112-maximize-expression-of-three-elements/maximize-expression-of-three-elements.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int maximizeExpressionOfThree(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        return nums[n-1] + nums[n-2] - nums[0];
+    
+    }
+}
