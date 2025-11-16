6cdf8f0c516d38b78f4ad880bf3cd4fab99e1946
kushkumar7668 <kushkumar16052004@gmail.com>
2025-11-17 02:43:39 +0530

    Time: 4 ms (89.84%) | Memory: 46.2 MB (15.51%) - LeetSync

---
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
@@ -0,0 +1,18 @@
+class Solution {
+    static final int M = 1_000_000_007;
+    
+    public int numSub(String s) {
+        long ans = 0;
+        long count1 = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '1') {
+                count1++;
+                // each count contribute no of substrings equal to count him self at that point 
+                ans = (ans + count1) % M; // add current streak
+            } else count1 = 0;
+        }
+
+        return (int) ans;
+    }
+}
