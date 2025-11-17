15c93d32c7fe381a3d8016734f2d4d1f6cefc307
kushkumar7668 <kushkumar16052004@gmail.com>
2025-11-17 21:15:36 +0530

    Time: 1 ms (99.71%) | Memory: 65.7 MB (5.71%) - LeetSync

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.java
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.java
@@ -5,7 +5,6 @@ class Solution {
         for(int i =0; i<nums.length; i++){
             if(nums[i] == 1 && curr == -1){
                 curr = i;
-                // prev = i;
             }
             else if(nums[i] == 1){
                 prev = curr;
