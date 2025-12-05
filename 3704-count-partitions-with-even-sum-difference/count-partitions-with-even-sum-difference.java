f8c67f858919cde886e340e8919531c638f8066d
kushkumar7668 <kushkumar16052004@gmail.com>
2025-12-06 00:46:05 +0530

    Time: 4 ms (6.35%) | Memory: 44 MB (6.04%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
@@ -1,15 +1,40 @@
 class Solution {
     public int countPartitions(int[] nums) {
-        int sum  = Arrays.stream(nums).sum();
-        int c =0;
-        int left = nums[0];
-        int right = Math.abs(sum - left);
-        for(int i =1; i<nums.length; i++){
-            if(Math.abs(right-left)%2 == 0) c++;
-            left += nums[i];
-            right = sum -left;
 
-        }
-        return c;
+        // int sum  = Arrays.stream(nums).sum();
+        // int c =0;
+        // int left = 0;
+        // int right = 0;
+        // for(int i =0; i<nums.length-1; i++){
+        //     left += nums[i];
+        //     right = sum -left;
+        //     if(Math.abs(right-left)%2 == 0) c++;
+
+
+        // }
+        // return c;
+
+
+
+        // suppose left sum is x and rightsum is y and total sum is s
+        /*  since    x + y = s    ------(i)   -> y = s-x
+        and we have to find ->  x - y  -----(ii)
+
+        put value of y ine eq (ii) then -
+
+            #)  x-y = x - (s-x)  -> 2x -s  ----(iii)
+
+
+            since 2x is always even , hence if s is even then all partions will hav evn difference  ans number of partions is n-1
+            */
+
+
+
+        // most optimal solution ->
+
+        int sum =  Arrays.stream(nums).sum();
+
+        return sum%2 == 0 ? nums.length-1 : 0;          
+
     }
 }
