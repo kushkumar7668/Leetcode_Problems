c5867b33c5c8bf6e807bcf257e98ab25730da12f
kushkumar7668 <kushkumar16052004@gmail.com>
2025-12-12 23:00:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (35.18%) - LeetSync

---
+++ 2031-egg-drop-with-2-eggs-and-n-floors/egg-drop-with-2-eggs-and-n-floors.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int twoEggDrop(int n) {
+        int ans = 0;
+        int d = 1;
+        while(n>0){
+            n = n-d;
+            ans += 1;
+            d++;
+        }
+        return ans;
+    }
+}
