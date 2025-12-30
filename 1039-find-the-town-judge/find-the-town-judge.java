98e5e0936dee3ba7e8bb19e2ab83ae76e19a6ae4
kushkumar7668 <kushkumar16052004@gmail.com>
2025-12-30 10:49:19 +0530

    Time: 2 ms (99.94%) | Memory: 53.6 MB (53.84%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -2,7 +2,7 @@ class Solution {
     public int findJudge(int N, int[][] trust) {
         int[] count = new int[N + 1];
         for (int[] t : trust) {
-            count[t[0]]--;
+            count[t[0]]--;  
             count[t[1]]++;
         }
         for (int i = 1; i <= N; ++i) {
