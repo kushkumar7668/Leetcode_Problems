f498cd151597e0768581b5b64093bbd1317e513b
kushkumar7668 <kushkumar16052004@gmail.com>
2026-01-15 14:56:49 +0530

    Time: 5 ms (60.29%) | Memory: 45.5 MB (26.47%) - LeetSync

--- 3214-maximize-area-of-square-hole-in-grid/maximize-area-of-square-hole-in-grid.java
+++ 3214-maximize-area-of-square-hole-in-grid/maximize-area-of-square-hole-in-grid.java
@@ -4,21 +4,30 @@ class Solution {
         Arrays.sort(vBars);
 
         int h = 0, ch = 0;
-        for(int i=1; i<hBars.length; i++){
-            if(hBars[i] == hBars[i-1]+1) ch++;
-            else ch = 0;   
-            h = Math.max(h, ch);
+        for (int i = 1; i < hBars.length; i++) {
+            if (hBars[i] == hBars[i - 1] + 1)
+                ch++;
+            else {
+                ch = 0;
+            }
+
+            h = Math.max(h, ch); //can't take Math.max() inside if or else as it eill not check maxlength at the end of array
         }
 
         int cv = 0, v = 0;
-        for(int i=1; i<vBars.length; i++){
-            if(vBars[i] == vBars[i-1]+1) cv++;
-            else cv =0;
+        for (int i = 1; i < vBars.length; i++) {
+            if (vBars[i] == vBars[i - 1] + 1)
+                cv++;
+            else {
+                cv = 0;
+            }
+
             v = Math.max(v, cv);
 
         }
 
-        return (int)Math.pow(Math.min(h+2,v+2), 2);
+        int side = Math.min(h + 2, v + 2);
+        return side * side;
 
     }
 }
