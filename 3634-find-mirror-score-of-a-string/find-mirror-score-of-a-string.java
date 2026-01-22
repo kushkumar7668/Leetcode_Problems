03a72e55991ec130883a97b180387ce6d68ee140
kushkumar7668 <kushkumar16052004@gmail.com>
2026-01-22 15:58:35 +0530

    Time: 25 ms (63.25%) | Memory: 46.7 MB (100.00%) - LeetSync

--- 3634-find-mirror-score-of-a-string/find-mirror-score-of-a-string.java
+++ 3634-find-mirror-score-of-a-string/find-mirror-score-of-a-string.java
@@ -1,15 +1,44 @@
 class Solution {
-    long calculateScore(String s) {
-    long ans = 0;
-    Map<Integer, List<Integer>> mp = new HashMap<>();
-    for(int i= 0; i < s.length(); ++i){
-        int cur = s.charAt(i) - 'a';
-        int mirror = 25 - cur;
-        if(mp.containsKey(mirror) && mp.get(mirror).size() > 0){
-            ans += (long)i - (long)mp.get(mirror).get(mp.get(mirror).size() - 1);
-            mp.get(mirror).remove(mp.get(mirror).size() - 1);
-        }else mp.computeIfAbsent(cur, key -> new ArrayList<>()).add(i);
+    public long calculateScore(String s) {
+
+        long score=0;
+        // this line creates an array of size 26 with a stack assigned to each index;
+        Stack<Integer>[] lastSeen= new Stack[26];
+        for(int i=0;i<26;i++){
+            lastSeen[i]=new Stack<>();
+        }
+
+        for(int i=0;i<s.length();i++){
+            char curr=s.charAt(i);
+            int currIdx=curr - 'a';
+            int mirrorIdx=25-currIdx;
+
+            if(!lastSeen[mirrorIdx].isEmpty()){
+                int prevIdx=lastSeen[mirrorIdx].pop();
+                score+= (i-prevIdx);
+            }
+            else{
+                lastSeen[currIdx].push(i);
+            }
+        }
+        return score;
     }
-    return ans;
 }
-}
+
+
+// class Solution {
+//     long calculateScore(String s) {
+//         long ans = 0;
+//         Map<Integer, List<Integer>> mp = new HashMap<>();
+//         for (int i = 0; i < s.length(); ++i) {
+//             int cur = s.charAt(i) - 'a';
+//             int mirror = 25 - cur;
+//             if (mp.containsKey(mirror) && mp.get(mirror).size() > 0) {
+//                 ans += (long) i - (long) mp.get(mirror).get(mp.get(mirror).size() - 1);
+//                 mp.get(mirror).remove(mp.get(mirror).size() - 1);
+//             } else
+//                 mp.computeIfAbsent(cur, key -> new ArrayList<>()).add(i);
+//         }
+//         return ans;
+//     }
+// }
