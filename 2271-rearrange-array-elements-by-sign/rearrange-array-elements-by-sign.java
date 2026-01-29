87882aaec66fa59f05c342ed0b9a9827a4bdbd96
kushkumar7668 <kushkumar16052004@gmail.com>
2026-01-29 14:33:18 +0530

    Time: 3 ms (99.29%) | Memory: 175.7 MB (36.02%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,16 +1,35 @@
+// class Solution {
+//     public int[] rearrangeArray(int[] nums) {
+//         Queue <Integer> pos = new LinkedList<>();
+//         Queue <Integer> neg = new LinkedList<>();
+//         for(int x : nums){
+//             if(x > 0) pos.offer(x);
+//             else neg.offer(x);
+//         }
+
+//         int[] ans = new int[nums.length];
+//         for(int i =0; i<ans.length; ){
+//             ans[i++] = pos.poll();
+//             ans[i++] = neg.poll(); 
+//         }
+//         return ans;
+//     }
+// }
+
+
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        Queue <Integer> pos = new LinkedList<>();
-        Queue <Integer> neg = new LinkedList<>();
-        for(int x : nums){
-            if(x > 0) pos.offer(x);
-            else neg.offer(x);
-        }
-
         int[] ans = new int[nums.length];
-        for(int i =0; i<ans.length; ){
-            ans[i++] = pos.poll();
-            ans[i++] = neg.poll(); 
+        int posIndex = 0 , negIndex = 1;
+        for(int x : nums){
+            if( x > 0){
+                ans[posIndex] = x;
+                posIndex += 2;
+            }
+            else{
+                ans[negIndex] = x;
+                negIndex += 2;
+            }
         }
         return ans;
     }
