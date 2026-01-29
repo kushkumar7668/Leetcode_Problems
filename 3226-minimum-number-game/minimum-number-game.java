08f611e8b2cb95b7656a0255c12e5578d59c10b7
kushkumar7668 <kushkumar16052004@gmail.com>
2026-01-29 15:23:40 +0530

    Time: 3 ms (95.74%) | Memory: 46.9 MB (77.02%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.java
+++ 3226-minimum-number-game/minimum-number-game.java
@@ -1,10 +1,32 @@
+// class Solution {
+//     public int[] numberGame(int[] nums) {
+//         Arrays.sort(nums);
+//         for(int i = 0; i<nums.length-1; i+=2){
+//             int temp = nums[i];
+//             nums[i] = nums[i+1]; 
+//             nums[i+1] = temp;
+//         }
+//         return nums;
+//     }
+// }
+
+
 class Solution {
     public int[] numberGame(int[] nums) {
-        Arrays.sort(nums);
-        for(int i = 0; i<nums.length-1; i+=2){
-            int temp = nums[i];
-            nums[i] = nums[i+1]; 
-            nums[i+1] = temp;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int n : nums) {
+            pq.offer(n);
+        }
+
+        int i=0;
+        while(pq.size()>1) {
+            int a = pq.poll();
+            int b = pq.poll();
+            nums[i++] = b;
+            nums[i++] = a;
+        }
+        if(!pq.isEmpty()) {
+            nums[nums.length-1] = pq.poll();
         }
         return nums;
     }
