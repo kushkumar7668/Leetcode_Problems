06b6209b12bf5a018f3bc63ae2cfbbf30e7fb2f4
kushkumar7668 <kushkumar16052004@gmail.com>
2026-02-11 14:14:04 +0530

    Time: 3 ms (99.66%) | Memory: 48.9 MB (93.14%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
@@ -1,32 +1,49 @@
 public class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        // Initialize count of subarrays, prefix sum, and hash map for remainders
-        int count = 0;
-        int prefixSum = 0;
-        HashMap<Integer, Integer> prefixMap = new HashMap<>();
-        prefixMap.put(0, 1); // To handle subarrays that start from the beginning
 
+        //  USING FREQUENCY array ->
+        int[] freq = new int[k];
+        int count = 0, rem = 0, sum = 0;
+        freq[0] = 1;
         for (int num : nums) {
-            // Update prefix sum
-            prefixSum += num;
-            
-            // Calculate the remainder of the prefix sum divided by k
-            int mod = prefixSum % k;
-            
-            // Adjust negative remainders to be positive
-            if (mod < 0) {
-                mod += k;
-            }
-            
-            // If this remainder has been seen before, it means there are subarrays ending here that are divisible by k
-            if (prefixMap.containsKey(mod)) {
-                count += prefixMap.get(mod);
-                prefixMap.put(mod, prefixMap.get(mod) + 1);
-            } else {
-                prefixMap.put(mod, 1);
-            }
+            sum += num;
+            rem = sum % k;
+            if (rem < 0)
+                rem += k;
+            count += freq[rem];
+            freq[rem]++;
         }
-        
-        return count; // Total number of subarrays divisible by k
+        return count;
+
+        // using HASHmap ->
+
+        // // Initialize count of subarrays, prefix sum, and hash map for remainders
+        // int count = 0;
+        // int prefixSum = 0;
+        // HashMap<Integer, Integer> prefixMap = new HashMap<>();
+        // prefixMap.put(0, 1); // To handle subarrays that start from the beginning
+
+        // for (int num : nums) {
+        //     // Update prefix sum
+        //     prefixSum += num;
+
+        //     // Calculate the remainder of the prefix sum divided by k
+        //     int mod = prefixSum % k;
+
+        //     // Adjust negative remainders to be positive
+        //     if (mod < 0) {
+        //         mod += k;
+        //     }
+
+        //     // If this remainder has been seen before, it means there are subarrays ending here that are divisible by k
+        //     if (prefixMap.containsKey(mod)) {
+        //         count += prefixMap.get(mod);
+        //         prefixMap.put(mod, prefixMap.get(mod) + 1);
+        //     } else {
+        //         prefixMap.put(mod, 1);
+        //     }
+        // }
+
+        // return count; // Total number of subarrays divisible by k
     }
 }
