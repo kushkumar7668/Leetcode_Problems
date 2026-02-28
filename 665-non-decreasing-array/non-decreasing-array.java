4e42787c49b7dede7a80957b2fbe767b4759f4d8
kushkumar7668 <kushkumar16052004@gmail.com>
2026-02-28 23:44:12 +0530

    Time: 1 ms (43.19%) | Memory: 47.2 MB (64.57%) - LeetSync

---
+++ 665-non-decreasing-array/non-decreasing-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int c = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] > nums[i]) {
+                c++;
+                if (c > 1)
+                    return false;
+                if (i == 1 || nums[i - 2] <= nums[i]) {
+                    nums[i - 1] = nums[i];
+                } else {
+                    nums[i] = nums[i - 1];
+                }
+            }
+        }
+
+        return true;
+    }
+}
