c8bf889d47c99bdca56f677a7e9f5a2cf2287eaa
kushkumar7668 <220946688+kushkumar7668@users.noreply.github.com>
2026-04-19 19:21:00 +0530

    Time: 3 ms (98.64%) | Memory: 90 MB (56.27%) - LeetSync

--- 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.java
+++ 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.java
@@ -1,26 +1,60 @@
-class Solution {
-    public int maxDistance(int[] nums1, int[] nums2) {
-        int ans = 0;
-        int n = nums2.length;
-        for(int i = 0; i<nums1.length; i++){
-            int idx = search(nums1, nums2, i, n-1);
-            ans = Math.max(ans, idx - i);
-        }
-        return ans;
-    }
+// class Solution {
+//     public int maxDistance(int[] nums1, int[] nums2) {
+//         int ans = 0;
+//         int n = nums2.length;
+//         for(int i = 0; i<nums1.length; i++){
+//             int idx = search(nums1, nums2, i);
+//             ans = Math.max(ans, idx - i);
+//         }
+//         return ans;
+//     }
 
-    public int search(int[] nums1, int[] nums2, int i, int j){
-        int idx = i;
-        int low = 0, high = nums2.length-1;
-        while(low <= high){
-            int mid = low + (high - low)/2;
-            if(nums1[i] <= nums2[mid]){
-                idx = mid;
-                low = mid+1;
+//     public int search(int[] nums1, int[] nums2, int i){
+//         int idx = i;
+//         int low = 0, high = nums2.length-1;
+//         while(low <= high){
+//             int mid = low + (high - low)/2;
+//             if(nums1[i] <= nums2[mid]){
+//                 idx = mid;
+//                 low = mid+1;
 
+//             }
+//             else high = mid-1;
+//         }
+//         return idx;
+//     }
+// }
+
+
+
+ /* OPTIMAL SOLUTION - Two Pointers
+     *
+     * Algorithm:
+     * 1. Use two pointers: i for nums1, j for nums2
+     * 2. If nums1[i] <= nums2[j]: valid pair, increment j
+     * 3. Else: increment i
+     * 4. Track maximum distance
+     *
+     * Time Complexity: O(n + m) - LINEAR!
+     * Space Complexity: O(1)
+     *
+     i          *
+     * {55, 30, 5, 4, 2};
+     j                   *
+     * {100, 20, 10, 10, 5};
+     */
+class Solution{
+    public static int maxDistance(int[] nums1, int[] nums2) {
+        int maxDist = 0;
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] <= nums2[j]) {
+                maxDist = Math.max(maxDist, j - i);
+                j++;
+            } else {
+                i++;
             }
-            else high = mid-1;
         }
-        return idx;
+        return maxDist;
     }
 }
