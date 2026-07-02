8c3ef2c63fc35fd24dd6b16688bf3f0898987720
kushkumar7668 <220946688+kushkumar7668@users.noreply.github.com>
2026-07-02 15:18:29 +0530

    Time: 47 ms (6.63%) | Memory: 48.8 MB (14.72%) - LeetSync

--- 575-distribute-candies/distribute-candies.java
+++ 575-distribute-candies/distribute-candies.java
@@ -1,11 +1,15 @@
 class Solution {
     public int distributeCandies(int[] candyType) {
-        int half = candyType.length / 2;
-        Set<Integer> set = new HashSet<Integer>();
-        for (int i = 0; i < candyType.length; i++) {
-            set.add(candyType[i]);
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int x : candyType){
+            map.put(x, map.getOrDefault(x,0)+1);
         }
-        return Math.min(half, set.size());
-        // maan lo ki set me sirf ek hi tarh ki candy ho toh fir n/2 candies toh khaa ji nhi sakta h because usko unique candies khaani h
+
+        int max = candyType.length/2;
+        int ans = 0;
+        for(int x : map.keySet()){
+            ans++;
+        }
+        return Math.min(max, ans);
     }
 }
